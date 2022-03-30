934ebb41834f4040ad27481d1becc4af07380333
DaliusGrigolius <dalius.grigolius@gmail.com>
2022-03-30 22:20:22 +0300

    second commit

---
+++ lesson 10.DataAccess/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using lesson_10.Business;
+using System;
+using System.Collections.Generic;
+
+namespace lesson_10.DataAccess
+{
+    public class ProductRepository
+    {
+        public List<Product> Retrieve()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product("produktas1", "nuostabus produktas", 12.99m));
+            products.Add(new Product("produktas2", "prastesnis produktas", 3.99m));
+
+            return products;
+        }
+    }
+}
---
+++ lesson 10/Program.cs
@@ -0,0 +1,34 @@
+﻿using lesson_10.Business;
+using lesson_10.DataAccess;
+using System;
+using System.Collections.Generic;
+
+namespace lesson_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa pasileidzia...");
+
+            ProductRepository prodRep = new ProductRepository();
+            List<Product> productsList = prodRep.Retrieve();
+
+            for (int i = 0; i < productsList.Count; i++)
+            {
+                Console.Write($"{productsList[i].Name}, {productsList[i].Description}, {productsList[i].CurrentPrice}\r\n");
+            }
+            //--------------------------------------------------------------------
+            // sukurti nauja uzsakyma ir atvaizduoti jo informacija su produktais
+            //--------------------------------------------------------------------
+
+            List<OrderProduct> products = new List<OrderProduct>();
+            Order newOrder = new Order(DateTime.Now, "Klaipedos r., Slengiu km., Isgalvota g. 4", products);
+            
+
+
+
+            Console.WriteLine("Programa pasibaigia...");
+        }
+    }
+}
